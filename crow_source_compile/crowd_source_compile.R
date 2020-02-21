260dd050a779795655f8a45c19b5ae0c37fc8391
bnasr <bijan.s.nasr@gmail.com>
2020-02-21 14:41:29 -0700

    complied training data

--- crow_source_compile/crowd_source_compile.R
+++ crow_source_compile/crowd_source_compile.R
@@ -4,6 +4,11 @@ output_file <- '~/Projects/crowdSource/outputFile.txt'
 
 dt <- setDT(read.csv(output_file, col.names = c('time', 'user', 'id', 'file', 'question', 'value', 'comment')))
 dt[,file := paste0('/projects/phenocam', file)]
-write.csv(dt[user!='test' & user!='bijan' & value !=9 & question == 'Snow', .(file, value)], file = 'snow_training_data.csv', row.names = F)
-write.csv(dt[user!='test' & user!='bijan' & value !=9 & question == 'Cloud', .(file, value)], file = 'cloud_training_data.csv', row.names = F)
-write.csv(dt[user!='test' & user!='bijan' & value !=9 & question == 'Fog or Haze', .(file, value)], file = 'haze_training_data.csv', row.names = F)
+dt[,local:=paste0('images/', basename(as.character(file)))]
+write.csv(dt[user!='test' & user!='bijan' & value !=9 & question == 'Snow', .(files = local, label = value)], file = '/hdd/Projects/phenocam_scripts/crow_source_compile/snow_training_data.csv', row.names = F)
+write.csv(dt[user!='test' & user!='bijan' & value !=9 & question == 'Cloud', .(files = local, label = value)], file = '/hdd/Projects/phenocam_scripts/crow_source_compile/cloud_training_data.csv', row.names = F)
+write.csv(dt[user!='test' & user!='bijan' & value !=9 & question == 'Fog or Haze', .(files = local, label = value)], file = '/hdd/Projects/phenocam_scripts/crow_source_compile/haze_training_data.csv', row.names = F)
+
+file.copy(from = unique(gsub(dt$file, pattern = '/projects/phenocam/data/archive', replacement = '/data/thumbnails')), 
+          overwrite = F,
+          to = '/hdd/Projects/phenocam_scripts/crow_source_compile/thumbnails/')
---
+++ summarize_rois.R
@@ -0,0 +1,56 @@
+library(phenocamapi)
+library(xROI)
+library(jpeg)
+
+
+rois <- get_rois()
+phenos <- get_phenos()
+
+rois <- merge(rois, phenos[,.(site, site_type)])
+
+n <- nrow(rois)
+# i=1
+pb <- txtProgressBar(1, n, style = 3)
+for(i in 1:n){
+  site <- rois[i, site]
+  roi_name <- rois[i, roi_name]
+  site_type <- rois[i, site_type]
+  
+  roi <- parseROI(paste0('/data/archive/', site, '/ROI/', roi_name, '_roi.csv'))
+  startdate <- as.Date(sapply(roi$masks, FUN = function(x){x$startdate}))
+  enddate <- as.Date(sapply(roi$masks, FUN = function(x){x$enddate}))
+  
+  ts1 <- as.data.table(read.csv(paste0('/data/archive/', site, '/ROI/', roi_name, '_1day.csv'), header = T, skip =22))
+  ts1[, date := as.Date(date)]
+  ts3 <- as.data.table(read.csv(paste0('/data/archive/', site, '/ROI/', roi_name, '_3day.csv'), header = T, skip =22))
+  ts3[, date := as.Date(date)]
+  
+  clRange <- range(ts3$date)
+  
+  cli <- try(readJPEG(paste0('/data/archive/', site, '/ROI/', site, '-cli.jpg')))
+  if(class(cli)=='try-error') next()
+  clitxt <- as.data.table(read.csv(paste0('/data/archive/', site, '/ROI/', site, '-cli.txt')))
+  clitxt[, Date := as.Date(Date)]
+
+  w <- (clitxt$Date>=clRange[1]) & (clitxt$Date<=clRange[2]) 
+  
+  png(file = paste0('rois/ind/', roi_name, '.png'), width = 11, height = 5, res = 300, units = 'in')
+  par(mfrow = c(2,1), mar = c(0,0,0,0), oma = c(2,2,2,0))
+  plot(ts1$date, ts1$gcc_90, col = 'green', type = 'l', xaxs='i',yaxs='i',  xaxt = 'n')
+  lines(ts3$date, ts3$gcc_90, col = 'red', lty =2)
+  abline(v = startdate, col = 'yellow')
+  abline(v = enddate, col = 'yellow')
+  
+  legend('topright', legend = c('1-day', '3-day'), col = c('green', 'red'), lty = 1:2)
+  plot(ts3$date, ts3$gcc_90, col = 'green', type = 'n', xaxs='i',yaxs='i', yaxt = 'n')
+  usr <- par()$usr
+  if(any(w)) rasterImage(cli[,w,], usr[1], usr[3], usr[2], usr[4])
+  abline(v = startdate, col = 'yellow')
+  abline(v = enddate, col = 'yellow')
+  
+  mtext(paste(roi_name, '   Type = ', site_type), outer = T)
+  dev.off()
+  
+  setTxtProgressBar(pb, i)
+}
+
